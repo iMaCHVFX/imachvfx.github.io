b3f2d0b28ed9003058c82b9547f77ccb27c46425
imach <imach@Gunch.Gunch.Gunch>
2024-03-17 16:28:53 -0700

    this is started to get inefficient

--- schedule.js
+++ schedule.js
@@ -1,8 +1,13 @@
+var currentdate = new Date();
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
 	background(195);
-	ellipse(100,100,100,100);
+	fill(255);
+	stroke(0);
+	text(currentdate.getMonth() + 1, 100, 100);
+
 }
