e496941abaeee45e5e372eebd06c82128da69fea
imach <imach@Gunch.Gunch.Gunch>
2024-03-17 18:11:56 -0700

    automatic

--- schedule.js
+++ schedule.js
@@ -1,20 +1,34 @@
 var currentdate = new Date();
 
+var displayDate = currentdate.getFullYear() + "/" + 
+				 (currentdate.getMonth() + 1) + "/" +
+				  currentdate.getDate() + "\n" +
+				  currentdate.getHours() + ":" +
+				  currentdate.getMinutes() + ":" +
+				  currentdate.getSeconds();
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	var currenteverything = currentdate.getFullYear() + "/" + 
-							(currentdate.getMonth() + 1) + "/" + 
-		                    currentdate.getDay + " " +
-		                    currentdate.getHour + ":" +
-		                    currentdate.getMinute + ":";
 }
 
 function draw() {
-	background(195);
+	displayDate = currentdate.getFullYear() + "/" + 
+				 (currentdate.getMonth() + 1) + "/" +
+				  currentdate.getDate() + "\n" +
+				  currentdate.getHours() + ":" +
+				  currentdate.getMinutes() + ":" +
+				  currentdate.getSeconds();
+	currentdate = new Date();
+	debugD = currentdate.getDate();
+	debugH = currentdate.getHours();
+	debugM = currentdate.getMinutes();
+	debugS = currentdate.getSeconds();
+	background(30);
 	fill(255);
 	stroke(0);
-	textSize(100);
-	text(currenteverything, windowWidth / 2, windowHeight / 2);
-	
+	textSize(20)
+	text(displayDate, windowWidth / 2, windowHeight / 2);
 
 }
+
+
